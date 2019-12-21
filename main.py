01ab384545c6cebcd0337e55584eddf763148593
shaoyu0966 <b06705028@ntu.edu.tw>
2019-12-21 17:59:52 +0800

    step by step variable

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import math
 from image import take_pictures, mapping
 from connect import connect2Arm
 
-
 # [[x],[y],[1]]
 # mapping
 # actual = [[-132.0847, 236.6651, 91.935104], [390.27371,411.77688, 574.30101], [1,1,1]]
 # img = [[469.51339957488125, 77.10519952646561,230.15167559590046], [131.64767848311172, 142.55809317365495, 315.3116258908931], [1,1,1]]
 # A = mapping(actual, img)
 A = np.load('img2actual.npy')
-print(A)
+print('A', A)
 number_of_objects = 3
+step_by_step = False
 
 if __name__ == "__main__":
+    
     mc, p_angle = take_pictures()
-
     s = connect2Arm()
-    ## Move to above object 1
+    
     for i in range(1, number_of_objects):
 
         img_p = [[mc[i][0]], [mc[i][1]], [1]]
@@ -28,13 +28,15 @@ if __name__ == "__main__":
         p_hat = np.reshape(p_hat, 3)
         print(img_p, p_hat)
         val = "MOVP " + str(p_hat[0]) +" "+ str(p_hat[1]) + " 0 " + str(-p_angle[i]) +" 0 180"
-        print(val)
-        input()
+        if step_by_step:
+            print(val)
+            input()
         s.sendall(val.encode('ascii'))
 
         val = "MOVP " + str(p_hat[0]) +" "+ str(p_hat[1]) + " -200 " + str(-p_angle[i]) +" 0 180"
-        print(val)
-        input()
+        if step_by_step:
+            print(val)
+            input()
         s.sendall(val.encode('ascii'))
 
         val = "OUTPUT 48 ON"
@@ -42,8 +44,9 @@ if __name__ == "__main__":
 
 
         val = "MOVP " + str(p_hat[0]) +" "+ str(p_hat[1]) + " 0 " + str(-p_angle[i]) +" 0 180"
-        print(val)
-        input()
+        if step_by_step:
+            print(val)
+            input()
         s.sendall(val.encode('ascii'))
 
         img_p = [[mc[0][0]], [mc[0][1]], [1]]
@@ -52,24 +55,28 @@ if __name__ == "__main__":
         print(p_hat)
 
         if i == 2:
-            val = "MOVP " + str(p_hat[0]) +" "+ str(p_hat[1]) + " -122 " + str(-p_angle[0]) +" 0 180"
+            val = "MOVP " + str(p_hat[0]) +" "+ str(p_hat[1]) + " -120 " + str(-p_angle[0]) +" 0 180"
         else:
-            val = "MOVP " + str(p_hat[0]) +" "+ str(p_hat[1]) + " -160 " + str(-p_angle[0]) +" 0 180"
-        print(val)
-        input()
+            val = "MOVP " + str(p_hat[0]) +" "+ str(p_hat[1]) + " -155 " + str(-p_angle[0]) +" 0 180"
+        if step_by_step:
+            print(val)
+            input()
         s.sendall(val.encode('ascii'))
 
         val = "OUTPUT 48 OFF"
-        input()
+        if step_by_step:
+            input()
         s.sendall(val.encode('ascii'))
 
 
         val = "MOVP " + str(p_hat[0]) +" "+ str(p_hat[1]) + " 0 " + str(-p_angle[0]) +" 0 180"
-        print(val)
-        input()
+        if step_by_step:
+            print(val)
+            input()
         s.sendall(val.encode('ascii'))
 
-    input()
+    if step_by_step:
+        input()
     goHome = "GOHOME"
     s.sendall(goHome.encode('ascii'))
     data = s.recv(1024)
